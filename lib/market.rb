764e8fddc46eefedf87ffa7e87b818f8fe3352b0
Logan Wilson <loganwilson777@gmail.com>
2023-03-07 11:09:32 -0700

    Adds Iteration 3 Methods

--- lib/market.rb
+++ lib/market.rb
@@ -17,5 +17,27 @@ class Market
   def vendors_that_sell(item)
     @vendors.select { |vendor| vendor.inventory.include?(item) }
   end
+
+  def sorted_item_list
+    items = @vendors.flat_map { |vendor| vendor.inventory.keys }
+    items.uniq.sort_by(&:name)
+  end
+
+  def total_inventory
+    inventory = Hash.new { |hash, key| hash[key] = { quantity: 0, vendors: [] } }
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        inventory[item][:quantity] += quantity
+        inventory[item][:vendors] << vendor
+      end
+    end
+    inventory
+  end
+
+  def overstocked_items
+    items_over_50 = total_inventory.select { |item, data| data[:quantity] > 50 }
+    items = items_over_50.keys
+    items.select { |item| vendors_that_sell(item).size > 1 }
+  end
 end
 
